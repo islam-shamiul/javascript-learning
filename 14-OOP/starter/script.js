c71be52622c8cb210504a5a37c65e50ab94268de
islam-shamiul <islamshamiul7@gmail.com>
2022-01-24 15:35:07 +0600

    Coding challenge 1 is solved

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,25 @@
 'use strict';
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.acceleration = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going at ${this.speed} km/h`);
+};
+
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
+
+bmw.acceleration();
+bmw.acceleration();
+bmw.brake();
+
+mercedes.acceleration();
+mercedes.acceleration();
+mercedes.brake();
