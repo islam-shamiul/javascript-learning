d8337df4c0f8810a616b9d3bd9c2b31ffd2ed0dc
islam-shamiul <islamshamiul7@gmail.com>
2022-02-15 12:26:49 +0600

    DOM manupulation selecting,creating,deleting,style attributes and classes added

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -29,3 +29,69 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+////////////////////////////////////////////////////
+///////////////////////////////////////////////////
+
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSelector = document.querySelectorAll('.section');
+console.log(allSelector); //returns NodeList
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons); //returns HTML Collection
+
+console.log(document.getElementsByClassName('btn'));
+
+//creating and iserting HTML elements
+//we can insert by .insertAdjecntHTML
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+//message.textContent = "We use cookies for improved functionality"
+message.innerHTML =
+  "We use cookies for improved functionality.<button class = 'btn btn--close-cookie'>Got it!</button>";
+// header.prepend(message);
+header.append(message);
+
+//delete element
+
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+  });
+
+//Styles
+message.style.backgroundColor = '#348595';
+message.style.width = '120%';
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 10 + 'px';
+
+//changing css property
+document.documentElement.style.setProperty('--color-primary', 'lightblue');
+
+//attributes
+
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+logo.alt = 'Beautiful minimalist logo';
+
+//non-standard
+console.log(logo.desinger);
+console.log(logo.getAttribute('desinger'));
+console.log(logo.getAttribute('src'));
+
+// data attributes
+console.log(logo.dataset.versionNumber);
